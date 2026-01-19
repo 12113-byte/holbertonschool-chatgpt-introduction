d4e84ce59b5d135e24d6c13c6b692add7ef8fc12
Joleen <12113@holbertonstudents.com>
2026-01-19 13:16:18 +1100

    Task 1 chatgpt

---
+++ debugging/factorial.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+if len(sys.argv) < 2:
+    print(f"Usage: {sys.argv[0]} <non-negative integer>")
+    sys.exit(1)
+
+f = factorial(int(sys.argv[1]))
+print(f)
